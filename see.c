94484dea6a38dbc20035ba6ce11341fa5a2f4f9c
Richie Finegan <richiefinegan1990@gmail.com>
2022-03-03 19:05:40 +0000

    Added C basics

--- see.c
+++ see.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-    char name[20];
-    printf("What is your name? ");
-    scanf("%s", name);
-    printf("Hello there, %s", name);
+int main() 
+{
+    int age = 50;
+
+    int* p = &age;
+
+    printf("The address of the age is %p", p);
+
+    printf("\nThe variable is %i", *p);
+    
+    
+}
+
+#include <stdio.h>
+//the following code shows how to access the memory address of some data
+int main() {
+  //first, we store some value on a variable
+  int firstValue = 200;
+
+  //we then print this value and its memory address by using the ampersand
+  printf("firstValue contains %i and is stored at the memory address %p", firstValue, &firstValue);
+
+  return 0;
 }
